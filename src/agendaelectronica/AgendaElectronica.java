520fb9a3d439b609bbada1025718600bb982ef50
Gonzalo Galarza <104328544+Gonzalogalarzaok@users.noreply.github.com>
2024-02-23 19:18:51 -0300

    Agenda Electronica
    Agenda Electrónica con Interfaz Gráfica realizada con java

---
+++ src/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,13 @@
+package agendaelectronica;
+
+import agendaelectronica.igu.Principal;
+
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal(); 
+        princ.setVisible(true); //lo hace visible
+        princ.setLocationRelativeTo(null); //lo pone en el medio
+    }
+    
+}
